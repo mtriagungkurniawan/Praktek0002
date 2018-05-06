30b43f26b1366b5230c65805e8fb775771996a15
M.Triagung Kurniawan <m.triagungkurniawan@gmail.com>
2018-05-06 15:16:59 +0700

    Membuat Attribute dan Behavior

--- src/praktek0002/Kain.java
+++ src/praktek0002/Kain.java
@@ -1,4 +1,12 @@
 package praktek0002;
 public class Kain {
+    double panjang;
+    double lebar;
     
+    void cetakinfo() {
+        System.out.println("====================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar :"+lebar);
+        System.out.println("====================");
+    }
 }
